2178929e2d893b7998e1f0e32411edc69bbab07d
alpingold <alpinnist448@gmail.com>
2025-12-09 19:10:34 -0500

    added battle-styles that controls playerBox and enemyBox in battlescene. cleaned up battlescene. Added a silly defeat animation, would also like to darken/make the image opaque but seems hard.

--- src/main/java/com/example/cardgamedemo/BattleController.java
+++ src/main/java/com/example/cardgamedemo/BattleController.java
@@ -1,21 +1,30 @@
 package com.example.cardgamedemo;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BattleController implements Initializable {
+    @FXML
+    private Label playerHealth;
 
     @FXML
     private Button attackButton;
 
     @FXML
     private Label enemyHealth;
+    @FXML
+    private ImageView enemyPicture;
 
     private Player player;
     private Enemy enemy;
@@ -29,6 +38,7 @@ public class BattleController implements Initializable {
         twoDmgCard = new AttackCard("Bash",2);
 
         updateEnemyHealthLabel();
+        updatePlayerHealthLabel();
     }
     @FXML
     void attackAction(ActionEvent event) {
@@ -36,12 +46,21 @@ public class BattleController implements Initializable {
         updateEnemyHealthLabel();
 
         if(enemy.isDead()){
+            defeatAnimation();
             attackButton.setDisable(true);
             enemyHealth.setText("Enemy HP: 0 You win!");
         }
     }
+    private void defeatAnimation(){
+        RotateTransition rotate = new RotateTransition(Duration.millis(250), enemyPicture);
+        rotate.setByAngle(15);
+        rotate.setCycleCount(1);
+        rotate.setAutoReverse(false);
+        rotate.play();
+    }
     private void updateEnemyHealthLabel(){
         enemyHealth.setText("Enemy HP: "+enemy.getCurrentHealth());
     }
+    private void updatePlayerHealthLabel(){playerHealth.setText("Your HP: "+player.getCurrentHealth());}
 
 }
