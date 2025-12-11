c7a5a8f5478141f9bedc48a0420595d94c93af2a
alpingold <alpinnist448@gmail.com>
2025-12-11 11:50:02 -0500

    ok scaling works now (kinda)

--- src/main/java/com/example/cardgamedemo/Main.java
+++ src/main/java/com/example/cardgamedemo/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,23 +13,35 @@ import static javafx.application.Application.launch;
 
 public class Main extends Application {
     private static Stage mainStage;
-
+    private static StackPane wrapper;
+    private static final double BASE_WIDTH = 1920;
+    private static double BASE_HEIGHT = 1080;
     @Override
     public void start(Stage stage) throws Exception{
         mainStage = stage;
         Parent root = FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
-        Scene scene = new Scene(root);
-        stage.setFullScreen(true);
-        stage.setResizable(false);
+        wrapper = new StackPane(root);
+        Scene scene = new Scene(wrapper, BASE_WIDTH, BASE_HEIGHT);
+        bindScaling(root,scene);
+        stage.setFullScreen(false);
+        stage.setMaximized(true);
+        stage.setResizable(true);
         stage.setTitle("Ethan's Card Game");
         stage.setScene(scene);
         stage.show();
     }
 
+    private static void bindScaling(Parent root, Scene scene) {
+        root.scaleXProperty().bind(scene.widthProperty().divide(BASE_WIDTH));
+        root.scaleYProperty().bind(scene.heightProperty().divide(BASE_HEIGHT));
+    }
+
     public static void switchScene(String fxmlFile){
         try{
             Parent newRoot = FXMLLoader.load(Main.class.getResource(fxmlFile));
-            mainStage.getScene().setRoot(newRoot);
+            Scene scene = mainStage.getScene();
+            wrapper.getChildren().setAll(newRoot);
+            bindScaling(newRoot,scene);
         } catch(IOException e){
             System.out.println("it broke idk");
         }
